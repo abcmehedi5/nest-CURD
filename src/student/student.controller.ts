2e75bda99248b1de3f0b4002ffaa09d12d4f089d
abcmehedi5 <abcmehedi5@gmail.com>
2023-10-02 15:27:21 +0600

    studnet delete operation

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -10,6 +10,7 @@ import {
   Res,
   Put,
   Param,
+  Delete,
 } from '@nestjs/common';
 
 @Controller('student')
@@ -41,11 +42,19 @@ export class StudentController {
   //   get all student
   @Get()
   async getAllStudent(@Res() response) {
+  try {
     const studentData = await this.studentService.getAllStudents();
     return response.status(HttpStatus.OK).json({
       message: 'all student data found successfull',
       studentData,
     });
+  } catch (error) {
+    return response.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: 400,
+        message: 'error student not found ',
+        error: 'Bad Request',
+      });
+  }
   }
 
   //update student data
@@ -55,13 +64,39 @@ export class StudentController {
     @Param('id') studnetId: string,
     @Body() updateStudentDto: UpdateStudentDto,
   ) {
-    const exestingStudent = await this.studentService.updateStudent(
-      studnetId,
-      updateStudentDto,
-    );
-    return response.status(HttpStatus.OK).json({
-      message: 'Student update successfull successfull',
-      exestingStudent,
-    });
+    try {
+        const exestingStudent = await this.studentService.updateStudent(
+            studnetId,
+            updateStudentDto,
+          );
+          return response.status(HttpStatus.OK).json({
+            message: 'Student update successfull successfull',
+            exestingStudent,
+          });
+    } catch (error) {
+        return response.status(HttpStatus.BAD_REQUEST).json({
+            statusCode: 400,
+            message: 'error student update not found',
+            error: 'Bad Request',
+          });
+    }
+  }
+
+  //student delete
+  @Delete('/:id')
+  async studentDelete(@Res() response, @Param('id') studentId: string) {
+    try {
+      const deleteStudent = await this.studentService.deleteStudent(studentId);
+      return response.status(HttpStatus.OK).json({
+        message: 'Student delete  successfull',
+        deleteStudent,
+      });
+    } catch (error) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: 400,
+        message: 'error student not delete ',
+        error: 'Bad Request',
+      });
+    }
   }
 }
